98a717bccaf9ba9c99b57638fe241a0a5223b6aa
javapagar <apariciogarcia.j@gmail.com>
2021-05-25 21:38:52 +0200

    first-project

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,5 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path='/img')
+    abstract=models.CharField(max_length=255)
+    repository=models.CharField(max_length=200)
